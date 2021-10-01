c08b72d97e9190bc77f2d21c943f6b30029b9edf
Mohiro <mohiro@bk.ru>
2021-10-01 23:14:07 +0300

    Оптимизация rate-button

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -2,35 +2,32 @@ var $ = require( "jquery" );
 
 $('.rate-button__star-1').on('click', function(){
   $('.rate-button input:checkbox').prop('checked', false);
-  $(".rate-button__star-1").prop('checked', true);
+  for (var i = 1; i <= 1; i++) {
+    $(".rate-button__star-" + i).prop('checked', true);
+  }
 });
 
 $('.rate-button__star-2').on('click', function(){
   $('.rate-button input:checkbox').prop('checked', false);
-  $(".rate-button__star-1").prop('checked', true);
-  $(".rate-button__star-2").prop('checked', true);
+  for (var i = 1; i <= 2; i++) {
+    $(".rate-button__star-" + i).prop('checked', true);
+  }
 });
 
 $('.rate-button__star-3').on('click', function(){
   $('.rate-button input:checkbox').prop('checked', false);
-  $(".rate-button__star-1").prop('checked', true);
-  $(".rate-button__star-2").prop('checked', true);
-  $(".rate-button__star-3").prop('checked', true);
+  for (var i = 1; i <= 3; i++) {
+    $(".rate-button__star-" + i).prop('checked', true);
+  }
 });
 
 $('.rate-button__star-4').on('click', function(){
   $('.rate-button input:checkbox').prop('checked', false);
-  $(".rate-button__star-1").prop('checked', true);
-  $(".rate-button__star-2").prop('checked', true);
-  $(".rate-button__star-3").prop('checked', true);
-  $(".rate-button__star-4").prop('checked', true);
+  for (var i = 1; i <= 4; i++) {
+    $(".rate-button__star-" + i).prop('checked', true);
+  }
 });
 
 $('.rate-button__star-5').on('click', function(){
-  $('.rate-button input:checkbox').prop('checked', false);
-  $(".rate-button__star-1").prop('checked', true);
-  $(".rate-button__star-2").prop('checked', true);
-  $(".rate-button__star-3").prop('checked', true);
-  $(".rate-button__star-4").prop('checked', true);
-  $(".rate-button__star-5").prop('checked', true);
+  $('.rate-button input:checkbox').prop('checked', true);
 });
