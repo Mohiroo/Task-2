20176d5825629b31aa383339a0ff5c98408106f8
Mohiroo <mohiro@bk.ru>
2021-11-18 04:47:07 +0300

    Добавлен feedback в components + небольшие фиксы других компонентов

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -2,11 +2,13 @@ var $ = require( "jquery" );
 var like_count = parseInt($(".like-button").text())
 
 $(".like-button").on('click', function(){
-  $(".like-button").toggleClass("like-button_enable")
-
   if ($(".like-button").hasClass("like-button_enable")) {
-    $(".like-button__count").text(like_count+1)
-  } else {
+    like_count -= 1;
     $(".like-button__count").text(like_count)
-  }  
+  } else {
+    like_count += 1;
+    $(".like-button__count").text(like_count)
+  }
+  
+  $(".like-button").toggleClass("like-button_enable")
 })
