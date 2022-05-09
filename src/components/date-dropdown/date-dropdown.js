ab88f773c24c267cef1caf3ae39a095e9c349ee9
Mohiroo <mohiro@bk.ru>
2022-05-09 13:17:18 +0300

    Добавлено: анимация нажатия на кнопку в date-dropdown

---
+++ src/components/date-dropdown/date-dropdown.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+const dateDropdownButton = $('.date-dropdown__button');
+const dateDropdownButtonActive = 'date-dropdown__button-active';
+
+dateDropdownButton.on("click", function() {
+  dateDropdownButton.toggleClass(dateDropdownButtonActive);
+});
