9b37e6d87c60ef42a46909e24173f8cc51517476
Mohiroo <mohiro@bk.ru>
2022-05-09 13:56:07 +0300

    Добавлено: анимация нажатия на кнопку и комментарии в dropdown-guests. Изменено: рефакторинг в dropdown-guests

--- src/components/dropdown-guests/dropdown-guests.js
+++ src/components/dropdown-guests/dropdown-guests.js
@@ -1,72 +1,99 @@
-let $ = require( "jquery" );
+import $ from 'jquery';
 
-let guests_buttons = $(".guests-menu__button");
-let guests_selector = $(".dropdown-guests__selector");
-let guests_menu = $(".dropdown-guests__menu");
-let button_open = $(".dropdown-guests__button");
-let button_clear = $(".guests__button-clear")
-let button_apply = $(".guests__button-apply")
+const guestsButtons = $(".guests-menu__button");
+const guestsSelector = $(".dropdown-guests__selector");
+const guestsMenu = $(".dropdown-guests__menu");
+const buttonOpen = $(".dropdown-guests__button");
+const buttonClear = $(".guests__button-clear");
+const buttonApply = $(".guests__button-apply");
 
-let mainText = $(".dropdown-guests__text")
+const dropdownGuestsButton = $('.dropdown-guests__button');
+const dropdownGuestsButtonActive = 'dropdown-guests__button-active';
 
-let adultButtonMinus = $(".adult__minus");
-let adultCount = $(".adult__count");
-let adultButtonPlus = $(".adult__plus")
+const mainText = $(".dropdown-guests__text");
 
-let childrenButtonMinus = $(".children__minus");
-let childrenCount = $(".children__count");
-let childrenButtonPlus = $(".children__plus");
+const adultButtonMinus = $(".adult__minus");
+const adultCount = $(".adult__count");
+const adultButtonPlus = $(".adult__plus");
 
-let infantsButtonMinus = $(".infants__minus");
-let infantsCount = $(".infants__count");
-let infantsButtonPlus = $(".infants__plus");
+const childrenButtonMinus = $(".children__minus");
+const childrenCount = $(".children__count");
+const childrenButtonPlus = $(".children__plus");
+
+const infantsButtonMinus = $(".infants__minus");
+const infantsCount = $(".infants__count");
+const infantsButtonPlus = $(".infants__plus");
 
 let adult = 0;
 let children = 0;
 let infants = 0;
 
-button_open.on('click', function(){
-  createMenu()
+// Создание меню
+buttonOpen.on('click', function () {
+  createMenu();
 });
 
-adultButtonMinus.on('click', function(){ minusCount("adult") })
-adultButtonPlus.on('click', function(){ plusCount("adult")})
-childrenButtonMinus.on('click', function(){ minusCount("children") })
-childrenButtonPlus.on('click', function(){ plusCount("children")})
-infantsButtonMinus.on('click', function(){ minusCount("infants") })
-infantsButtonPlus.on('click', function(){ plusCount("infants")})
-
-guests_buttons.on('click', function(){
-  let people = adult + children + infants
-  
-  if (people == 0) {
-    button_clear.addClass("guests__button-disable")
-  } else {
-    button_clear.removeClass("guests__button-disable")
-  }
-
-  getText()
+// Кнопки по категориям
+adultButtonMinus.on('click', function () {
+  minusCount("adult");
+});
+adultButtonPlus.on('click', function () {
+  plusCount("adult");
+});
+childrenButtonMinus.on('click', function () {
+  minusCount("children");
+});
+childrenButtonPlus.on('click', function () {
+  plusCount("children");
+});
+infantsButtonMinus.on('click', function () {
+  minusCount("infants");
+});
+infantsButtonPlus.on('click', function () {
+  plusCount("infants");
 });
 
-button_apply.on('click', function() {
-  guests_selector.toggleClass(" dropdown-guests__selector-active")
-  guests_menu.toggleClass(" dropdown-guests__menu-active")
+// Подтверждение введенных данных
+buttonApply.on('click', function () {
+  dropdownGuestsButton.toggleClass(dropdownGuestsButtonActive);
 
-  getText()
-})
+  guestsSelector.toggleClass(" dropdown-guests__selector-active");
+  guestsMenu.toggleClass(" dropdown-guests__menu-active");
 
-button_clear.on("click", function() {
+  getText();
+});
+
+// Очистка меню
+buttonClear.on("click", function () {
   refreshMenu();
-})
+});
 
+// Отображать или нет кнопку очистки меню
+guestsButtons.on('click', function () {
+  if (adult + children + infants == 0) {
+    buttonClear.addClass("guests__button-disable");
+  } else {
+    buttonClear.removeClass("guests__button-disable");
+  };
+
+  getText();
+});
+
+// Положение по умолчаю кнопки меню
+dropdownGuestsButton.on("click", function () {
+  dropdownGuestsButton.toggleClass(dropdownGuestsButtonActive);
+});
+
+// Функция - создание меню
 function createMenu() {
-  guests_selector.toggleClass(" dropdown-guests__selector-active")
-  guests_menu.toggleClass(" dropdown-guests__menu-active")
+  guestsSelector.toggleClass("dropdown-guests__selector-active");
+  guestsMenu.toggleClass("dropdown-guests__menu-active");
 
-  adultCount.text(adult)
-  childrenCount.text(children)
-  infantsCount.text(infants)
+  adultCount.text(adult);
+  childrenCount.text(children);
+  infantsCount.text(infants);
 
+  // Установка неактивных кнопок по категориям
   if (adult == 0) {
     adultButtonMinus.addClass("guests__minus-inactive");
   }
@@ -75,141 +102,142 @@ function createMenu() {
   }
   if (infants == 0) {
     infantsButtonMinus.addClass("guests__minus-inactive");
-  }
-  
-  getText()
+  };
+
+  getText();
 }
 
+// Функция - очистка меню
 function refreshMenu() {
   adult = 0;
   children = 0;
   infants = 0;
 
   mainText.text("Сколько гостей");
-  
-  adultCount.text(adult)
-  childrenCount.text(children)
-  infantsCount.text(infants)
+
+  adultCount.text(adult);
+  childrenCount.text(children);
+  infantsCount.text(infants);
 
   if (adult == 0) {
     adultButtonMinus.addClass("guests__minus-inactive");
-  }
+  };
   if (children == 0) {
     childrenButtonMinus.addClass("guests__minus-inactive");
-  }
+  };
   if (infants == 0) {
     infantsButtonMinus.addClass("guests__minus-inactive");
-  }
-}
+  };
 
+  buttonClear.addClass("guests__button-disable");
+};
+
+// Функция - кнопка-минус по категориям
 function minusCount(category) {
   if (category == "adult") {
     if (adult > 0) {
-      adult = adult - 1
+      adult--;
       adultCount.text(adult);
-    }
+    };
     if (adult == 0) {
       adultButtonMinus.addClass("guests__minus-inactive");
-    }
+    };
   } else if (category == "children") {
     if (children > 0) {
-      children = children - 1
+      children--;
       childrenCount.text(children);
-    }
+    };
     if (children == 0) {
       childrenButtonMinus.addClass("guests__minus-inactive");
-    }
+    };
   } else if (category == "infants") {
     if (infants > 0) {
-      infants = infants - 1
+      infants--;
       infantsCount.text(infants);
-    }
+    };
     if (infants == 0) {
       infantsButtonMinus.addClass("guests__minus-inactive");
-    }
-  }
-}
+    };
+  };
+};
 
+// Функция - кнопка-плюс по категориям
 function plusCount(category) {
   if (category == "adult") {
     if (adult == 0) {
       adultButtonMinus.removeClass("guests__minus-inactive");
-    }
+    };
 
-    adult = adult + 1;
-
+    adult++;
     adultCount.text(adult);
   } else if (category == "children") {
     if (children == 0) {
       childrenButtonMinus.removeClass("guests__minus-inactive");
-    }
+    };
 
-    children = children + 1;
-
+    children++;
     childrenCount.text(children);
   } else if (category == "infants") {
-    if (infants == 0) {
-      infantsButtonMinus.removeClass("guests__minus-inactive");
-    }
-
-    infants = infants + 1;
-
-    infantsCount.text(infants);
-  }
-}
-
-function getText () {
-  let guestCount = adult + children
-
-  if (guestCount > 0) {
-    if (infants > 0) {
-      mainText.text(`${guestCount} ${getNoun("guest", guestCount)}, ${infants} ${getNoun("no-guest", infants)}`);
-    } else {
-      mainText.text(`${guestCount} ${getNoun("guest", guestCount)}`);
-    }
-  } else {
-    mainText.text("Сколько гостей")
-  }
-}
-
-function getNoun(isGuest, count) {
-  if (isGuest === "guest") {
-    let countArray = String(count).split('')
-    let lastNumber = parseInt(countArray[countArray.length - 1])
+    if (adult !== 0) {
+      if (infants == 0) {
+        infantsButtonMinus.removeClass("guests__minus-inactive");
+      };
   
-    if (lastNumber === 1) {
-      if (count == 11) {
-        return "гостей"
-      } else {
-        return "гость"
-      }
-    } else if (lastNumber > 1 && lastNumber < 5 ) {
-      if (count > 11 && count < 16) {
-        return "гостей"
-      } else {
-        return "гостя"
-      }
-    } else  {
-      return "гостей"
-    }
-  } else {
-    let countArray = String(count).split('')
-    let lastNumber = parseInt(countArray[countArray.length - 1])
+      infants++;
+      infantsCount.text(infants);
+    };
+  };
+};
 
+// Тест в строке меню
+function getText() {
+  let guestCount = adult + children;
+
+  if (guestCount > 0 && infants > 0) {
+    mainText.text(`${guestCount} ${getNoun("guest", guestCount)}, ${infants} ${getNoun("no-guest", infants)}`);
+  } else if (guestCount > 0 && infants === 0) {
+    mainText.text(`${guestCount} ${getNoun("guest", guestCount)}`);
+  } else {
+    mainText.text("Сколько гостей");
+  };
+}
+
+// Существительное относительно количества гостй
+function getNoun(isGuest, count) {
+  let countArray = String(count).split('');
+  let lastNumber = parseInt(countArray[countArray.length - 1]);
+
+  if (isGuest === "guest") {
     if (lastNumber === 1) {
       if (count == 11) {
-        return "младенцев"
+        return "гостей";
       } else {
-        return "младенец"
-      }
-    } else if (lastNumber > 1 && lastNumber < 5 ) {
+        return "гость";
+      };
+    } else if (lastNumber > 1 && lastNumber < 5) {
       if (count > 11 && count < 16) {
-        return "младенцев"
+        return "гостей";
       } else {
-        return "младенца"
+        return "гостя";
+      };
+    } else {
+      return "гостей";
+    };
+  } else {
+    if (lastNumber === 1) {
+      if (count == 11) {
+        return "младенцев";
+      } else {
+        return "младенец";
+      };
+    } else if (lastNumber > 1 && lastNumber < 5) {
+      if (count > 11 && count < 16) {
+        return "младенцев";
+      } else {
+        return "младенца";
       }
     } else {
-      return "младенцев"
-    }
-  }
-}
+      return "младенцев";
+    };
+  };
+};
