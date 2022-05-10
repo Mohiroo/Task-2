23492c4dd409dc718cdc46b4a34350f39b0e991f
Mohiroo <mohiro@bk.ru>
2022-05-11 00:02:50 +0300

    Изменено: структура expandable-checkbox-list

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,9 +1,11 @@
-let $ = require( "jquery" );
+import $ from 'jquery';
 
-let expandable_button = $(".expandable-checkbox__button")
-let expandable_list = $(".expandable-checkbox__list")
+const expandableToggle = $(".expandable-checkbox__toggle");
+const expandableToggleArrow = $('.expandable-checkbox__toggle-button');
+const expandableToggleActive = 'expandable-checkbox__toggle-button-active';
+const expandableList = $(".expandable-checkbox__list");
 
-expandable_button.on('click', function(){
-  expandable_list.toggleClass("expandable-checkbox__list_enable")
-  expandable_button.toggleClass("expandable-checkbox__button_enable")
-})
+expandableToggle.on('click', function () {
+  expandableToggleArrow.toggleClass(expandableToggleActive);
+  expandableList.toggle();
+});
