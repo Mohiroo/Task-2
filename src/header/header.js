f8f29ba3a597f1069ac1e269870238ee94f79723
Mohiroo <mohiro@bk.ru>
2022-05-10 18:08:01 +0300

    Изменено: структура и позиционирование в header

--- src/header/header.js
+++ src/header/header.js
@@ -1,12 +1,8 @@
-let $ = require( "jquery" );
+import $ from "jquery";
 
-let header_button = $(".header__icon")
-let header_menu = $(".header__menu")
+const headerButton = $(".header__hamburger");
+const headerMenu = $(".header__menu");
 
-header_button.on("click", function() {
-  if (header_menu.css("display") == "none") {
-    header_menu.css("display", "block")
-  } else {
-    header_menu.css("display", "none")
-  }
-})
+headerButton.on("click", function() {
+  headerMenu.toggle();
+});
