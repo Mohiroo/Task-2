fd66ee32d003827e24b6c8baa2cb7ec38fe6db6c
Mohiroo <mohiro@bk.ru>
2022-05-20 19:35:49 +0300

    Изменено: рефакторинг expandable-checkbox-list

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
 
-const expandableToggle = $(".expandable-checkbox__toggle");
-const expandableToggleArrow = $('.expandable-checkbox__toggle-button');
-const expandableToggleActive = 'expandable-checkbox__toggle-button-active';
+const expandableToggle = $(".expandable-checkbox__menu");
+const expandableButton = $('.expandable-checkbox__button');
+const expandableButtonActive = 'expandable-checkbox__button-active';
 const expandableList = $(".expandable-checkbox__list");
 
 expandableToggle.on('click', function () {
-  expandableToggleArrow.toggleClass(expandableToggleActive);
+  expandableButton.toggleClass(expandableButtonActive);
   expandableList.toggle();
 });
