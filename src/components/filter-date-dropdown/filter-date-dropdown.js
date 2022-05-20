8be93529cb2ab5fd8fe50d32a3623fbb5f80ac27
Mohiroo <mohiro@bk.ru>
2022-05-20 18:43:27 +0300

    Добавлено: анимация, взаимодействие Изменено: структруа

---
+++ src/components/filter-date-dropdown/filter-date-dropdown.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+const filterDateButton = $('.filter-date__button');
+const filterDateButtonActive = 'filter-date__button-active';
+const calendarButtonApply = $(".calendar__button-apply");
+
+filterDateButton.on('click', function () {
+  filterDateButton.toggleClass(filterDateButtonActive);
+});
+
+calendarButtonApply.on('click', function () {
+  filterDateButton.removeClass(filterDateButtonActive)
+});
