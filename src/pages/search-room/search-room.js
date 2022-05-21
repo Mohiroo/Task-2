a362cd2d5a7bca61dae2a892995c67b53ddd3593
Mohiroo <mohiro@bk.ru>
2022-05-22 02:37:34 +0300

    Добавлено: страница search-room Изменено: функционал, масштабирование, взаимодействие, рефакторинг

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,35 +1,28 @@
+import $ from "jquery";
+import "../../header/header";
 import "../../cards/calendar/calendar";
 import "../../cards/room-card/room-card";
-import "../../components/dropdown-guests/dropdown-guests"
+import "../../components/filter-date-dropdown/filter-date-dropdown";
+import "../../components/dropdown-guests/dropdown-guests";
 import "../../components/range-slider/range-slider.js";
 import "../../components/dropdown-rooms/dropdown-rooms";
 import "../../components/expandable-checkbox-list/expandable-checkbox-list";
-import "../../header/header";
+import "../../components/pagination/pagination";
 
-let $ = require( "jquery" );
+const filters = $(".search-room__filters");
+const filtersButton = $(".filters__button");
+const translateFilters = () => $(window).width() < 1190 ? -294 : 0;
+let translateValue = -294;
 
-let filter_button = $(".filters__button")
-let filters_list = $(".filters__list")
-
-let caledarButton = $(".filter-date-dropdown__button")
-let calendar = $(".calendar")
-
-caledarButton.on('click', function() {
-  calendar.toggleClass("calendar-active")
-})
-
-filter_button.on("click", function() {
-  if (filters_list.css("display") == "none") {
-    filters_list.css("display", "block")
-  } else {
-    filters_list.css("display", "none")
-  }
-})
-
-$(window).resize(function () {
-  if ($(window).width() > 940) {
-    filters_list.css("display", "block")
-  } else {
-    filters_list.css("display", "none")
-  }
+window.addEventListener('load', function () {
+    filters.css('transform', `translateX(${translateFilters()}px)`);
 });
+
+window.addEventListener('resize', function () {
+  filters.css('transform', `translateX(${translateFilters()}px)`);
+});
+
+filtersButton.on("click", function () {
+  filters.css('transform', `translateX(${translateValue = translateValue === 0 ? -294 : 0}px)`);
+})
+
