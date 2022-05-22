84ff3da927678f698c79ec10ef21aa87a8bf2308
Mohiroo <mohiro@bk.ru>
2022-05-22 21:15:05 +0300

    Изменено: анимация кнопки перенесена в calendar

--- src/components/date-dropdown/date-dropdown.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-const dateDropdownButton = $('.date-dropdown__button');
-const dateDropdownButtonActive = 'date-dropdown__button-active';
-
-dateDropdownButton.on("click", function () {
-  dateDropdownButton.toggleClass(dateDropdownButtonActive);
-});
