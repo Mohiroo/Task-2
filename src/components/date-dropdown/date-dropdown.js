1f3994665d0cd4700ad4f9d48ccbf365b6069073
Mohiroo <mohiro@bk.ru>
2022-05-22 18:07:10 +0300

    Изменено: рефакторинг date-dropdown

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -3,6 +3,6 @@ import $ from 'jquery';
 const dateDropdownButton = $('.date-dropdown__button');
 const dateDropdownButtonActive = 'date-dropdown__button-active';
 
-dateDropdownButton.on("click", function() {
+dateDropdownButton.on("click", function () {
   dateDropdownButton.toggleClass(dateDropdownButtonActive);
 });
