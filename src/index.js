33ea448ea1c84d2362e2e7359735ed4e0054ceb6
Mohiroo <mohiro@bk.ru>
2022-05-24 20:27:26 +0300

    Добавлено: шрифты

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import "./styles.scss";
 import "./index.pug";
-import "./pages/search-room/search-room.js"
 
-// import "./components/range-slider/range-slider.js";
-// import "./components/expandable-checkbox-list/expandable-checkbox-list";
-// import "./components/like-button/like-button";
-// import "./components/text-field/text-field.js";
-// import "./components/rate-button/rate-button.js";
-// import "./components/dropdown-rooms/dropdown-rooms";
-// import "./components/dropdown-guests/dropdown-guests";
-// import "./cards/room-card/room-card";
-// import "./cards/find-room/find-room";
-// import "./cards/book-card/book-card";
-// import "./header/header.js"
+// import "./pages/sign-in/sign-in"
+// import "./components/rate-button/rate-button";
